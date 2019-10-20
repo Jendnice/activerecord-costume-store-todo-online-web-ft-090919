a990345eae3b232f331d7c9a3f30646b566eeea8
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-20 17:38:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       hh.string :name 
       hh.string :location
       hh.string :theme 
-      hh.string :price 
+      hh.float :price 
       hh.boolean :family_friendly
       hh.datetime :opening_date
       hh.datetime :closing_date
