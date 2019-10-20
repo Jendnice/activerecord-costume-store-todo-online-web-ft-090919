193021255cbe20d3525e800f4128783fab023564
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-20 17:23:04 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,16 @@
 # Create your costume_stores migration here
 
-class CreateCostumeStores
+class CreateCostumeStores <ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :costume_stores do |cs|
+      cs.string :name 
+      cs.string :location
+      cs.integer :costume_inventory
+      cs.integer :number_of_employees
+      cs.string :still_in_business
+      cs.string :opening_time
+      cs.string :closing_time 
+  end 
   
 end 
