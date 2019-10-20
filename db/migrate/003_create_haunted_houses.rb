329b5ac294ca110b8cb940136416eb13e855d1c4
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-20 17:36:48 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,16 @@
 # Create your haunted_houses migration here
 
-class HauntedHouse < ActiveRecord::Base 
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :haunted_houses do |hh|
+      hh.string :name 
+      hh.string :location
+      hh.string :theme 
+      hh.string :price 
+      hh.boolean :family_friendly
+      hh.timedate :opening_date
+      hh.timedate :
+  end 
+  
 end 
